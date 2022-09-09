5119e3fcc03e452064bfb91af7768154b45b548c
hariprasathmohan <h9042717477@gmail.com>
2022-09-09 23:12:15 +0530

    Fresh

--- app/src/main/java/com/example/fbproject/LoginActivity.kt
+++ app/src/main/java/com/example/fbproject/LoginActivity.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONObject
-import java.io.IOException
-import java.net.URI
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         var signupButton = findViewById<Button>(R.id.signup_btn)
+        var loginButton = findViewById<Button>(R.id.login_btn)
         signupButton.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+            //login("temp@temp.com","temp")
+        }
+        loginButton.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             //login("temp@temp.com","temp")
--- app/src/main/java/com/example/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fragments/HomeFragment.kt
@@ -1,15 +1,16 @@
 package com.example.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.adapter.HomeAdapter
-import com.example.fbproject.MainActivity
 import com.example.fbproject.R
+import jp.wasabeef.richeditor.RichEditor
 
 
 class HomeFragment : Fragment() {
@@ -24,9 +25,29 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_home, container, false)
+        richTextEditer(view)
         var list : RecyclerView = view.findViewById(R.id.list)
         list.layoutManager = LinearLayoutManager(activity)
         list.adapter = HomeAdapter()
         return  view
+    }
+    fun richTextEditer (view: View){
+        var mEditor = view.findViewById<RichEditor>(R.id.editor)
+        mEditor.setEditorHeight(200);
+        mEditor.setEditorFontSize(22);
+        mEditor.setPadding(10, 10, 10, 10);
+        view.findViewById<ImageButton>(R.id.action_undo).setOnClickListener { mEditor.undo() }
+        view.findViewById<ImageButton>(R.id.action_redo).setOnClickListener { mEditor.redo() }
+        view.findViewById<ImageButton>(R.id.action_bold).setOnClickListener { mEditor.setBold() }
+        view.findViewById<ImageButton>(R.id.action_italic).setOnClickListener { mEditor.setItalic() }
+        view.findViewById<ImageButton>(R.id.action_strikethrough).setOnClickListener { mEditor.setStrikeThrough() }
+        view.findViewById<ImageButton>(R.id.action_underline).setOnClickListener { mEditor.setUnderline() }
+        view.findViewById<ImageButton>(R.id.action_heading1).setOnClickListener { mEditor.setHeading(1) }
+        view.findViewById<ImageButton>(R.id.action_heading2).setOnClickListener { mEditor.setHeading(2) }
+        view.findViewById<ImageButton>(R.id.action_heading3).setOnClickListener { mEditor.setHeading(3) }
+        view.findViewById<ImageButton>(R.id.action_align_left).setOnClickListener { mEditor.setAlignLeft() }
+        view.findViewById<ImageButton>(R.id.action_align_center).setOnClickListener { mEditor.setAlignCenter() }
+        view.findViewById<ImageButton>(R.id.action_align_right).setOnClickListener { mEditor.setAlignRight() }
+        view.findViewById<ImageButton>(R.id.action_insert_bullets).setOnClickListener { mEditor.setBullets() }
     }
 }
